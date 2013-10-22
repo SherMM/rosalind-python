f4caa242a3790b24efeea29b47026584be13e013
SherMM <helioblast86@gmail.com>
2013-10-22 13:24:03 -0500

    textbook problem 3

---
+++ text_1C.py
@@ -0,0 +1,16 @@
+def dataParse(file_name):
+	file = open(file_name, 'r')
+	data = [line.strip() for line in file.readlines()]
+	return data
+	
+def patternMatch(data):
+	import re
+	pattern = data[0]
+	strand = data[1]
+	match_pos = [m.start(0) for m in re.finditer("%s(?=%s)" % (pattern[0],pattern[1:]), strand)]
+	return match_pos
+	
+def patternPrinter(matches):
+	for match in matches:
+		print match,
+	print 
