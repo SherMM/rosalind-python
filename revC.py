93836a50eee26ab40655ac8e6190fdf9c65d88f5
SherMM <helioblast86@gmail.com>
2013-11-06 19:42:58 -0600

    added text description for reverse compliment program or revC.py

--- revC.py
+++ revC.py
@@ -1,3 +1,10 @@
+'''
+This is a program to solve problem REVC from the rosalind.info site.
+This program finds the reverse compliment for a DNA strand.
+For example: is the DNA strand is AAAACCCGGT, then its reverse
+compliment will be ACCGGGTTTT. 
+'''
+
 def dataParse(file_name):
 	file = open(file_name, 'r')
 	data = [line.strip() for line in file.readlines()][0][::-1]
